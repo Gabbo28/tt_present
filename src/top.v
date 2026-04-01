fd79b71d5a9e7bda4342f8bcf55e3c14e7b5511b
Gabriele Bocca <b.gabri@gmail.com>
2026-04-01 20:22:56 +0200

    massive cleanup

--- src/top.v
+++ src/top.v
@@ -9,39 +9,32 @@ module top (
     input wire [7:0] data_in,   // inputs, 8 bits
     input wire [1:0] trigger,   // HW trojan trigger
 
-// could add 3 output bits for key_full, data_in_block_full, data_out_block_empty(for reading)
     output reg [7:0] data_out,  // outputs, 8 bits
     output wire data_top_valid  // data ready for reading
 );
 
 // internal registers & wires
-reg [79:0] key;         //key register
-reg [63:0] data_in_block;  // data_in block
-reg [63:0] data_out_block;  // data_out block
-reg load_state; //state register for load operation
-reg [2:0] state; // state for state machine
-reg [7:0] reading; // reading state register
+reg [79:0] r_key;       // key register
+reg [63:0] r_data_in;   // data_in register
+reg [63:0] r_data_out;  // data_out register
+reg [2:0] state;        // state for state machine
+reg [7:0] reading;      // reading state register
+reg r_load;             // register for loading into present_encrypt module
 
-wire [79:0] key_w;
-wire [63:0] data_in_w;
-wire [63:0] data_out_w;
-wire load_w;
-wire data_out_valid;
+wire data_out_valid;    // data out "is valid" wire
+wire [63:0] w_data_out; // data_out wire
 
-assign key_w = key;
-assign data_in_w = data_in_block;
-assign load_w = load_state;
 assign data_top_valid = state[2];
 
 // instanciate present_encrypt module
 present_encrypt i_present_encrypt (
-    .idat(data_in_w),
-    .key(key_w),
-    .load(load_w),
+    .idat(r_data_in),
+    .key(r_key),
+    .load(r_load),
     .clk(clk),
     .rst_n(rst_n),
     .trigger(trigger),
-    .odat(data_out_w),
+    .odat(w_data_out),
     .odat_valid(data_out_valid));
 
 // state enum
@@ -54,38 +47,37 @@ localparam KEY_IN = 1,
 always @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
         state <= 0;
-        key <= 0;
-        data_in_block <= 0;
-        data_out_block <= 0;
-        load_state <= 0;
-        data_out <= 0;
+        r_key <= 0;
+        r_data_in <= 0;
+        r_data_out <= 0;
         reading <= 0;
+        r_load <= 0;
     end else begin
-        state <= {reading[7], cmd};
         reading <= {reading[6:0], 1'b0};
+        state <= {reading[7], cmd};
         case(state)
             //IDLE: nop
             KEY_IN: begin
-                load_state <= 0;
-                key <= {key[71:0], data_in}; //load byte into key reg
+                r_load <= 0;
+                r_key <= {r_key[71:0], data_in};            // load byte into key reg
             end
             DATA_IN: begin 
-                load_state <= 0;
-                data_in_block <= {data_in_block[55:0], data_in}; // load byte into data_in_block register
+                r_load <= 0;
+                r_data_in <= {r_data_in[55:0], data_in};    // load byte into r_data_in register
             end
-            LOAD: load_state <= 1; 
-            READ_BYTE: begin //read byte from data_out_block
-                load_state <= 0;
-                data_out <= data_out_block[63:56];
-                data_out_block <= {data_out_block[55:0], 8'b0};
+            LOAD: r_load <= 1; 
+            READ_BYTE: begin    // read byte from r_data_out
+                r_load <= 0;
+                data_out <= r_data_out[63:56];
+                r_data_out <= {r_data_out[55:0], 8'b0};
             end
             default: begin
-                load_state <=0; //IDLE STATE, same as "0"
+                r_load <= 0;    // IDLE state
             end
         endcase
         if (data_out_valid) begin
             reading <= 8'b1111_1111;
-            data_out_block <= data_out_w;      
+            r_data_out <= w_data_out;      
         end
     end
 end
